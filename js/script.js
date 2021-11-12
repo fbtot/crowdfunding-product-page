11eea4e7d9aa89d7cd51ff931e82df623e772ffd
Filippo Bristot <filippo.bristot@hotmail.it>
2021-11-12 13:26:35 +0100

    feat(js): add bookmark button funcionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+/* ========================== § DOM ELEMENTS === */
+const bookmarkBtn = document.getElementById("bookmark-btn");
 
+/* ========================== § BOOKMARK BUTTON === */
+bookmarkBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  bookmarkBtn.classList.toggle("bookmarked");
+});
