dd6e982ed42b8e885c6608e3227cf29cce9e84f8
Filippo Bristot <filippo.bristot@hotmail.it>
2021-11-13 15:54:11 +0100

    refactor(js): format according to eslint style

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 /* ========================== § DOM ELEMENTS === */
-const bookmarkBtn = document.getElementById("bookmark-btn");
+const bookmarkBtn = document.getElementById('bookmark-btn');
 
-const headerNavContainer = document.getElementById("header-nav-container");
-const hamburgerBtn = document.getElementById("hamburger-btn");
-const headerNav = document.getElementById("header-nav");
+const headerNavContainer = document.getElementById('header-nav-container');
+const hamburgerBtn = document.getElementById('hamburger-btn');
+const headerNav = document.getElementById('header-nav');
 
 /* ========================== § BOOKMARK BUTTON === */
-bookmarkBtn.addEventListener("click", function (e) {
+bookmarkBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  bookmarkBtn.classList.toggle("bookmarked");
+  bookmarkBtn.classList.toggle('bookmarked');
 });
 
 /* ========================== § HEADER NAV === */
-hamburgerBtn.addEventListener("click", function (e) {
+hamburgerBtn.addEventListener('click', (e) => {
   e.preventDefault();
-  headerNavContainer.classList.toggle("open");
+  headerNavContainer.classList.toggle('open');
 
-  headerNav.classList.toggle("focus-in");
+  headerNav.classList.toggle('focus-in');
 });
