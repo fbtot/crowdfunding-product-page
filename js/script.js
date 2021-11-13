9126d3c7b12284fe931822378f6193dd223e318e
Filippo Bristot <filippo.bristot@hotmail.it>
2021-11-13 13:38:13 +0100

    feat(js): add mobile menu functionality

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
 /* ========================== § DOM ELEMENTS === */
 const bookmarkBtn = document.getElementById("bookmark-btn");
 
+const headerNavContainer = document.getElementById("header-nav-container");
+const hamburgerBtn = document.getElementById("hamburger-btn");
+const headerNav = document.getElementById("header-nav");
+
 /* ========================== § BOOKMARK BUTTON === */
 bookmarkBtn.addEventListener("click", function (e) {
   e.preventDefault();
   bookmarkBtn.classList.toggle("bookmarked");
+});
+
+/* ========================== § HEADER NAV === */
+hamburgerBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  headerNavContainer.classList.toggle("open");
+
+  headerNav.classList.toggle("focus-in");
 });
